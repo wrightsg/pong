7d70902a82454eed0ceeedf6f4b9da401119a797
Simon Wright <wrightsgdev@gmail.com>
2018-09-12 00:18:23 +0200

    hello_world: keep window open until closed

--- src/hello_world.cpp
+++ src/hello_world.cpp
@@ -6,5 +6,14 @@ int main ()
 {
     sf::Window window(sf::VideoMode(800, 600), "sfml hello world");
 
+    while (window.isOpen()) {
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed) {
+                window.close();
+            }
+        }
+    }
+
     return EXIT_SUCCESS;
 }
